b5fa089354b9fdc4110f2a37e321cfaeac561f1a
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-01-31 16:14:20 -0500

    4:14; removing prefix

--- src/FunctionApps/PITest_FunctionApp/IngestFunction/__init__.py
+++ src/FunctionApps/PITest_FunctionApp/IngestFunction/__init__.py
@@ -285,7 +285,10 @@ async def use_asyncio(settings):
             all_files = await sftp.glob("/eICR/*")
             
             already_processed_path = files('IngestFunction') / 'already_processed.txt'
-            already_processed = already_processed_path.read_text().split(",")
+            
+            with open(already_processed_path, 'r') as f:
+                already_processed = f.read().split(",")
+                already_processed = [entry.removeprefix("220128") for entry in already_processed]
             target_files = set(all_files) - set(already_processed)
 
             logger.info(f"All files length: {len(all_files)},already_processed length: {len(already_processed)}, target_file length: {len(target_files)}. Sample entries. All_files: {all_files[0]}, already_processed: {already_processed[0]}")
