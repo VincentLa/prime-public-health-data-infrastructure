498d0e845b3018b60abe51da05438a3fe8267e03
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-01-30 23:32:47 -0500

    11:32; adding custom repr, removing dataclass

--- src/FunctionApps/PITest_FunctionApp/IngestFunction/settings.py
+++ src/FunctionApps/PITest_FunctionApp/IngestFunction/settings.py
@@ -1,12 +1,13 @@
 import os
-from dataclasses import dataclass
 
-@dataclass
 class Settings:
     hostname = os.environ.get("VDHSFTPHostname")
     username = os.environ.get("VDHSFTPUsername")
     password = os.environ.get("VDHSFTPPassword")
     connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
+
+def __repr__(self):
+    return repr(f"hostname: {self.hostname}, username: {self.username}, password: {self.password}, connection_string: {self.connection_string}")
 
 
 settings = Settings()
